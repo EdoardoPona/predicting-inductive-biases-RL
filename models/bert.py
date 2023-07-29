88d61c2cb204439bbed5bed06b344ca4c85f9056
eliasschmied <eliasschmied>
2023-07-29 17:17:30 +0100

    changed relative to absolute imports (wasn't working)

--- models/bert.py
+++ models/bert.py
@@ -2,7 +2,7 @@ from transformers import (
     BertForSequenceClassification,
     BertTokenizer,
 )
-from .base import BaseClassifier
+from base import BaseClassifier
 
 
 class BertClassifier(BaseClassifier):
