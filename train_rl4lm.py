78740c633dc70b3e2cdd6956e4a2d0d54aa20be6
Alex <alex@ip-172-31-39-49.eu-west-2.compute.internal>
2023-08-16 09:58:58 +0000

    loading untrained transformer

--- train_rl4lm.py
+++ train_rl4lm.py
@@ -13,26 +13,21 @@ from transformers import AutoModelForCausalLM
 
 RL_CONFIG = """
 tokenizer:
-  model_name: {model_path} 
+  model_name: test_model 
   padding_side: left
   truncation_side: left
   pad_token_as_eos_token: True
 
 reward_fn:
-  id: increasing_numbers
-  args:
-    min_tokens: 20
+  id: toy_reward
 
 datapool:
-  id: dummy_pool
-  args:
-    n_samples: 50
-    prompt: '<|endoftext|>'
+  id: toy_pool
       
 env:
   n_envs: 10
   args:
-    max_prompt_length: 5
+    max_prompt_length: 10
     max_episode_length: 20
     terminate_on_eos: True
 
@@ -51,21 +46,19 @@ alg:
   policy:
     id: causal_lm_actor_critic_policy
     args:
-      model_name: gpt2
+      model_name: random_gpt2
       apply_model_parallel: True
       generation_kwargs:
         do_sample: True
-        max_new_tokens: 20  #this must align with env's max steps
+        max_new_tokens: 3  #this must align with env's max steps
 
 train_evaluation:
   eval_batch_size: 256
-  n_iters: 100
-  eval_every: 5
-  save_every: 20
+  n_iters: 500
+  eval_every: 20
+  save_every: 100
   metrics:
-    - id: increasing_numbers
-      args:                                                                                                                                                            
-        min_tokens: 20                                                                                                                                                 
+    - id: toy_metric                                                                                                                                               
 """
 
 def main(
@@ -106,29 +99,52 @@ def make_model_config(path):
     can be loaded by AutoModel.from_pretrained. 
     This means we need to instantiate a model, and call .save_pretrained
     rather than saving the config directly '''
-    from transformers import GPT2Config
+    from transformers import GPT2Config, GPT2LMHeadModel
 
     config = GPT2Config(
         activation_function='gelu_new',
         n_head=4,
         n_layer=2,
+        n_ctx = 32,
         hidden_size=128,
-        n_positions=100,  # upper bound on max length of input
+        n_positions=32,  # upper bound on max length of input
+        vocab_size = 50000
     )
-    model = AutoModelForCausalLM.from_config(
-      config
-    )
-    assert type(model) == transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel, \
-        "Model is not of type GPT2LMHeadModel, is of type {}".format(type(model))
+    
+    import torch.nn as nn
+
+    class RandomGPT2(GPT2LMHeadModel):
+        def _init_weights(self, module):
+            if isinstance(module, (nn.Linear, nn.Embedding, nn.LayerNorm)):
+                module.weight.data.normal_(mean=0.0, std=0.02)
+                if isinstance(module, (nn.Linear, nn.LayerNorm)) and module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, nn.MultiheadAttention):
+                module.in_proj_weight.data.normal_(mean=0.0, std=0.02)
+
+
+    model = RandomGPT2(config)
+    model.save_pretrained('random_gpt2')
+
+    # print(f"model is {model}")
+
+    
+    # assert type(model) == transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel, \
+    #     "Model is not of type GPT2LMHeadModel, is of type {}".format(type(model))
     print('created model', model)
     model.save_pretrained(path)     
-    
 
 def make_tokenizer_config(path):
-    from transformers import GPT2Tokenizer 
-    # just going to use gpt2 default tokenizer for now 
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')  
-    tokenizer.save_pretrained(path)
+  from tokenizers import Tokenizer, models
+  number_tokenizer = Tokenizer(models.WordLevel(unk_token="[UNK]"))
+  n = 50000
+  new_tokens = [' ']
+  for x in range(n):
+      new_tokens.append(str(x))
+
+  number_tokenizer.add_tokens(new_tokens)
+  number_tokenizer.save(path)
+
 
 def make_train_config(model_path, train_config_path):
     rl_config = RL_CONFIG.format(model_path=model_path)
@@ -138,21 +154,23 @@ def make_train_config(model_path, train_config_path):
 if __name__ == "__main__":
     model_path = 'test_model'
     make_model_config(model_path)
-    make_tokenizer_config(model_path)
+    make_tokenizer_config(model_path + "/tokenizer_config.json")
 
     from transformers import AutoModelForCausalLM
     model = AutoModelForCausalLM.from_pretrained(model_path)
-    print(type(model))
-    print(model.num_parameters())
+    print(f"Actual model is {model}")
+    print(f"{type(model)=}")
+    print(f"{model.num_parameters()=}")
 
 
-    train_config_path = 'tests/rl_config.yaml'
+
+    train_config_path = 'tests/rl_config_copy.yaml'
     make_train_config(model_path, train_config_path)
 
     main(
         config_path=train_config_path,
         project_name='rl4lms',
-        experiment_name='test_experiment',
+        experiment_name='test_experiment3',
         base_path_to_store_results='tests/results',
         entity_name='test_user',
         log_to_wandb=False,
