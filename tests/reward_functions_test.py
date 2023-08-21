4800440e6f23d8877bdcbd9e160127bfbec2f2f2
EdoardoPona <edoardo.pona@gmail.com>
2023-08-21 13:06:28 +0100

    testing option c reward

--- tests/reward_functions_test.py
+++ tests/reward_functions_test.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import reward_functions
 
 from rl4lms.envs.text_generation.observation import Observation
+from rl4lms.envs.text_generation.toy_reward import AscendingDescendingReward
 
 # Testing LoveringToyTaskRewardFunction:
 
@@ -38,3 +39,8 @@ loveringtoyobs2 = Observation(
 @pytest.mark.parametrize("test_input,expected", [((loveringtoyobs1, '01040'), -5), ((loveringtoyobs1, '13112'), -8), ((loveringtoyobs2, '01040'), -6), ((loveringtoyobs2, '13112'), -3)])
 def test_loveringtoy(test_input, expected):
     assert reward_functions.LoveringToyTaskRewardFunction()(*test_input)==expected
+
+@pytest.mark.parametrize("gen_text, label, expected", [('1 2 3 0 0', 1, 3/5), ('1 2 3 0 0', 0, 1/5), ('6 5 4 3 8', 0, 4/5)])
+
+def test_ascending_descending_reward(gen_text, label, expected):
+    assert AscendingDescendingReward.reward(gen_text, label)==expected
