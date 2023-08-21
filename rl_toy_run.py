3154b71ff03b0838ece85cfa078b42fcadc10f01
Diogo Cruz <diogo.abc.cruz@gmail.com>
2023-08-21 23:48:59 +0000

    Cleaned up iteration.

--- rl_toy_run.py
+++ rl_toy_run.py
@@ -14,34 +14,35 @@ if __name__ == "__main__":
     n_layers = 2
     hidden_size = 256
 
-    rates = ["0", "0.01", "0.5"]
-    toys = [1, 2, 3, 5]     # NOTE we are skipping 4 right? 
+    #rates = ["0", "0.01", "0.5"]
+    rates = ["0", "0.001", "0.01", "0.05", "0.1", "0.2", "0.5"]
+    toys = [1, 2, 3, 5]     # we are skipping 4 
 
-    for r in rates:
-        for t in toys:
-          model_path = f'rl_results/test_model_toy{t}_rate{r}'
-          results_path = f'rl_results/results_toy{t}_rate{r}'
+    for t in toys:
+        for r in rates:
+            model_path = f'rl_results/test_model_toy{t}_rate{r}'
+            results_path = f'rl_results/results_toy{t}_rate{r}'
 
-          clear_dir(model_path)
-          clear_dir(results_path)
+            clear_dir(model_path)
+            clear_dir(results_path)
 
-          make_model_config(model_path, vocab_size, model_max_length, n_layers, hidden_size)
-          make_tokenizer_config(model_path, vocab_size, model_max_length)
+            make_model_config(model_path, vocab_size, model_max_length, n_layers, hidden_size)
+            make_tokenizer_config(model_path, vocab_size, model_max_length)
 
-          model = AutoModelForCausalLM.from_pretrained(model_path)
+            model = AutoModelForCausalLM.from_pretrained(model_path)
 
-          print('#'*100)
-          print(f"Training MODEL {model}")
-          print(f"{model.num_parameters()=}")
+            print('#'*100)
+            print(f"Training MODEL {model}")
+            print(f"{model.num_parameters()=}")
 
-          train_config_path = 'tests/rl_config.yaml'
-          make_train_config(model_path, prompt_length, episode_length, train_config_path, toy_data=t, rate=r)
+            train_config_path = 'tests/rl_config.yaml'
+            make_train_config(model_path, prompt_length, episode_length, train_config_path, toy_data=t, rate=r)
 
-          main(
-              config_path=train_config_path,
-              project_name='rl4lms',
-              experiment_name=f'toy_ep{episode_length}_l{n_layers}_h{hidden_size}_steps64',    # NOTE: make the steps a param 
-              base_path_to_store_results=results_path,
-              entity_name='diogocruz',
-              log_to_wandb=True,
-          )
+            main(
+                config_path=train_config_path,
+                project_name='rl4lms',
+                experiment_name=f'toy{t}_r{r}_ep{episode_length}_l{n_layers}_h{hidden_size}_steps64',    # NOTE: make the steps a param 
+                base_path_to_store_results=results_path,
+                entity_name='diogocruz',
+                log_to_wandb=True,
+            )
