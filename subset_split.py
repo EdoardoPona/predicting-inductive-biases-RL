8e9b1b981b9b363a6a06c32d76332d835b403a1f
Diogo Cruz <diogo.abc.cruz@gmail.com>
2023-08-22 17:03:49 +0000

    Updated to cover all toy cases.

--- subset_split.py
+++ subset_split.py
@@ -3,17 +3,19 @@
 import pandas as pd 
 import os 
 
-data_path = './properties/toy_1'
+data_path = './properties'
+tasks = ['toy_1', 'toy_2', 'toy_3', 'toy_5']  # List of tasks to process
 filename = 'test.tsv'
 
-#%%
-df = pd.read_csv(os.path.join(data_path, filename), sep='\t')
-df
-
-#%%
-for sub in df.groupby('section'):
-    section, sub_df = sub
-    sub_df.to_csv(os.path.join(data_path, f'test_{section}.tsv'), sep='\t', index=False)
-
-# %%
+for task in tasks:
+    task_path = os.path.join(data_path, task)
+    
+    # Read the data
+    df = pd.read_csv(os.path.join(task_path, filename), sep='\t')
+    print(f"Processing {task}...")
+    
+    # Split the data and save to separate files
+    for sub in df.groupby('section'):
+        section, sub_df = sub
+        sub_df.to_csv(os.path.join(task_path, f'test_{section}.tsv'), sep='\t', index=False)
 
