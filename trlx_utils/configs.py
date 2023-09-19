8e52ff63cf6180758dc64221b2814653779d3d56
EdoardoPona <edoardo.pona@gmail.com>
2023-09-19 09:40:44 +0100

    configs

--- trlx_utils/configs.py
+++ trlx_utils/configs.py
@@ -21,20 +21,11 @@ from trlx.models.modeling_ppo import PPOConfig
 def default_config():
     return TRLConfig(
         train=TrainConfig(
-<<<<<<< HEAD
             seq_length=64,
             epochs=200,
             total_steps=4000,
             batch_size=16,
             checkpoint_interval=10000,
-=======
-            seed=1,
-            seq_length=64,
-            epochs=100,
-            total_steps=51200,
-            batch_size=64,
-            checkpoint_interval=51200,
->>>>>>> 76b4e79385e2ee131cf4535e467b41628db90c50
             eval_interval=51200,
             pipeline="PromptPipeline",
             trainer="AcceleratePPOTrainer",
@@ -65,13 +56,8 @@ def default_config():
         ),
         method=PPOConfig(
             name="PPOConfig",
-<<<<<<< HEAD
             num_rollouts=256,        # 128             # 1024
             chunk_size=8,
-=======
-            num_rollouts=512,                     # 
-            chunk_size=64,
->>>>>>> 76b4e79385e2ee131cf4535e467b41628db90c50
             ppo_epochs=4,                         # 
             init_kl_coef=0.005,
             target=None,                        # 6,
@@ -86,14 +72,9 @@ def default_config():
             ref_std=None,
             cliprange_reward=10,
             gen_kwargs=dict(
-<<<<<<< HEAD
                 temperature = 1.5,
                 max_new_tokens=40,
                 min_length=40, 
-=======
-                max_new_tokens=48,
-                min_length=48, 
->>>>>>> 76b4e79385e2ee131cf4535e467b41628db90c50
                 top_k=0,
                 top_p=1.0,
                 do_sample=True,
